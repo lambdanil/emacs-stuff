d5bc5621dc3025a68f1d1a42bdb399a008879c80
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-06-08 20:59:07 +0200

    update font

--- doom.d/config.el
+++ doom.d/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 18 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 18))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
