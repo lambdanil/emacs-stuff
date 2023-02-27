59e72ad079884343886887434c0fb80de6fee842
CuBeRJAN <shadenk30011@gmail.com>
2023-02-27 19:44:57 +0100

    config cleanup

--- configs-root/guix/etc/config.scm
+++ configs-root/guix/etc/config.scm
@@ -1,5 +1,8 @@
-;; https://github.com/CuBeRJAN/emacs-config
+;; ------------------------------------------
+;;  https://github.com/CuBeRJAN/emacs-config
+;; ------------------------------------------
 
+;; Import necessary modules ----------------------------------------------------
 (use-modules (gnu)
              (gnu services)
 	     (gnu services dbus)
@@ -11,12 +14,16 @@
 	     (gnu packages networking)
              (guix packages)
 	     (srfi srfi-1))
-(use-service-modules linux nix desktop networking ssh xorg)
+(use-service-modules linux desktop networking ssh xorg)
 (use-package-modules linux package-management)
+;; -----------------------------------------------------------------------------
 
-;; Enable nonguix substitutes
+
+;; Service modifications -------------------------------------------------------
 (define %my-services
   (modify-services %desktop-services
+
+		   ;; nonguix substitutes --------------------------------------
                    (guix-service-type config => (guix-configuration
                                                  (inherit config)
                                                  (substitute-urls
@@ -25,20 +32,35 @@
                                                  (authorized-keys
                                                   (append (list (local-file "./signing-key.pub"))
                                                           %default-authorized-guix-keys))))
-		   (dbus-root-service-type config =>
-                        (dbus-configuration (inherit config)
-                                 (services (list libratbag))))
-		   ))
+		   ;; ----------------------------------------------------------
 
+
+		   ;; additional dbus services ---------------------------------
+		   (dbus-root-service-type config =>
+		        (dbus-configuration (inherit config)
+			        (services (list libratbag))))
+		   ))
+                   ;; ----------------------------------------------------------
+
+
+;; -----------------------------------------------------------------------------
+
+
+;; OS config -------------------------------------------------------------------
 (operating-system
+
+ ;; Localization ---------------------------------------------------------------
  (locale "cs_CZ.utf8")
  (timezone "Europe/Prague")
  (keyboard-layout (keyboard-layout "cz"))
+ ;; ----------------------------------------------------------------------------
+
+ ;; Account and group configuration --------------------------------------------
  (host-name "eternity")
  (kernel linux)
  (groups 
    (cons* 
-     (user-group (name "games"))
+     (user-group (name "games")) ;; For libratbagd
      %base-groups))
  (initrd microcode-initrd)
  (firmware (list linux-firmware))
@@ -50,6 +72,9 @@
                 (supplementary-groups
                  '("wheel" "netdev" "audio" "video" "lp" "libvirt" "games")))
                %base-user-accounts))
+  ;; ---------------------------------------------------------------------------
+
+ ;; System packages ------------------------------------------------------------
  (packages
   (append
    (map specification->package
@@ -62,24 +87,30 @@
 	 "libratbag"
          "git"))
    %base-packages))
+  ;; ---------------------------------------------------------------------------
+
+ ;; System services ------------------------------------------------------------
  (services
   (cons*
    (append
     (list (service gnome-desktop-service-type)
           (bluetooth-service #:auto-enable? #f)
-	  (pam-limits-service
-           ;; For Lutris / Wine esync
+	  
+	  (pam-limits-service ;; For Lutris / Wine esync
            (list (pam-limits-entry "*" 'hard 'nofile 524288)))
-	  ;; For executing binaries
-          (extra-special-file "/lib64/ld-linux-x86-64.so.2"
-           (file-append glibc "/lib/ld-linux-x86-64.so.2"))
+	  
+          (extra-special-file "/lib64/ld-linux-x86-64.so.2" ;; For precompiled executing binaries
+			      (file-append glibc "/lib/ld-linux-x86-64.so.2"))
+	  
           (set-xorg-configuration
            (xorg-configuration
             (keyboard-layout keyboard-layout)))
+
           (service libvirt-service-type
                    (libvirt-configuration
                     (unix-sock-group "libvirt")
                     (tls-port "16555")))
+	  
           (service zram-device-service-type
                    (zram-device-configuration
                     (size "8172M")
@@ -89,6 +120,10 @@
 ;;	     (eq? (service-kind service) gdm-service-type))
 ;;	   %my-services)
    ))
+ ;; ----------------------------------------------------------------------------
+
+
+ ;; Bootloader and disk config -------------------------------------------------
  (bootloader
   (bootloader-configuration
    (bootloader grub-efi-bootloader)
@@ -107,10 +142,12 @@
            (uuid "D846-4633"
                  'fat32))
           (type "vfat"))
-	 (file-system
+	 (file-system ;; Second hard drive
           (mount-point "/mnt/media/jan/external")
           (device
            (uuid "6e008012-794a-40ad-99e9-69825235e4c5"
                  'ext4))
           (type "ext4"))
          %base-file-systems)))
+  ;; ---------------------------------------------------------------------------
+;; -----------------------------------------------------------------------------
