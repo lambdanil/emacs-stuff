5f721573c5059554fba705d65bcc428e58e3e675
CuBeRJAN <shadenk30011@gmail.com>
2023-03-03 02:56:14 +0100

    add docker, update VM config

--- configs-root/guix/etc/config.scm
+++ configs-root/guix/etc/config.scm
@@ -10,6 +10,7 @@
              (nongnu packages linux)
              (nongnu system linux-initrd)
              (gnu services virtualization)
+	     (gnu services docker)
              (gnu packages fonts)
 	     (gnu packages networking)
              (guix packages)
@@ -59,9 +60,9 @@
  (host-name "eternity")
  (kernel linux)
  (groups 
-   (cons* 
-     (user-group (name "games")) ;; For libratbagd
-     %base-groups))
+  (cons* 
+   (user-group (name "games")) ;; For libratbagd
+   %base-groups))
  (initrd microcode-initrd)
  (firmware (list linux-firmware))
  (users (cons* (user-account
@@ -70,7 +71,7 @@
                 (group "users")
                 (home-directory "/home/jan")
                 (supplementary-groups
-                 '("wheel" "netdev" "audio" "video" "lp" "libvirt" "games")))
+                 '("wheel" "netdev" "audio" "video" "lp" "libvirt" "kvm" "games" "docker")))
                %base-user-accounts))
   ;; ---------------------------------------------------------------------------
 
@@ -99,17 +100,25 @@
 	  (pam-limits-service ;; For Lutris / Wine esync
            (list (pam-limits-entry "*" 'hard 'nofile 524288)))
 	  
-          (extra-special-file "/lib64/ld-linux-x86-64.so.2" ;; For precompiled executing binaries
+          (extra-special-file "/lib64/ld-linux-x86-64.so.2" ;; For executing precompiled binaries
 			      (file-append glibc "/lib/ld-linux-x86-64.so.2"))
 	  
           (set-xorg-configuration
            (xorg-configuration
             (keyboard-layout keyboard-layout)))
 
+	  ;; VM stuff ----------------------------------------------------------
+	  (service virtlog-service-type
+		   (virtlog-configuration
+		    (max-clients 1000)))
+	  
           (service libvirt-service-type
                    (libvirt-configuration
                     (unix-sock-group "libvirt")
                     (tls-port "16555")))
+	  ;; -------------------------------------------------------------------
+
+	  (service docker-service-type)
 	  
           (service zram-device-service-type
                    (zram-device-configuration
@@ -128,26 +137,28 @@
   (bootloader-configuration
    (bootloader grub-efi-bootloader)
    (targets '("/boot/efi"))
+   (timeout 3)
    (keyboard-layout keyboard-layout)))
  (file-systems
-  (cons* (file-system
-          (mount-point "/")
-          (device
-           (uuid "fd02d6c6-5ff1-48d9-b9cb-a444ceb260dd"
-                 'ext4))
-          (type "ext4"))
-         (file-system
-          (mount-point "/boot/efi")
-          (device
-           (uuid "D846-4633"
-                 'fat32))
-          (type "vfat"))
-	 (file-system ;; Second hard drive
-          (mount-point "/mnt/media/jan/external")
-          (device
-           (uuid "6e008012-794a-40ad-99e9-69825235e4c5"
-                 'ext4))
-          (type "ext4"))
-         %base-file-systems)))
-  ;; ---------------------------------------------------------------------------
+  (cons*   
+   (file-system
+     (mount-point "/")
+     (device
+      (uuid "fd02d6c6-5ff1-48d9-b9cb-a444ceb260dd"
+            'ext4))
+     (type "ext4"))
+   (file-system
+     (mount-point "/boot/efi")
+     (device
+      (uuid "D846-4633"
+            'fat32))
+     (type "vfat"))
+   (file-system ;; Second hard drive
+     (mount-point "/mnt/media/jan/external")
+     (device
+      (uuid "6e008012-794a-40ad-99e9-69825235e4c5"
+            'ext4))
+     (type "ext4"))
+   %base-file-systems)))
+;; ----------------------------------------------------------------------------
 ;; -----------------------------------------------------------------------------
---
+++ extra/distrobox-docker.scm
@@ -0,0 +1,32 @@
+(define-module (gnu packages distrobox-docker)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages)
+  #:use-module (guix packages)
+  #:use-module (gnu packages docker)
+  #:use-module (guix utils)
+  #:use-module (guix build utils)
+  #:use-module (gnu packages containers))
+
+(define-public distrobox-docker
+  (package
+    (inherit distrobox)
+    (name "distrobox-docker")
+    (inputs (modify-inputs (package-inputs distrobox)
+			   (append docker)
+			   (append docker-cli)
+			   (delete "podman")))
+    (arguments
+     (substitute-keyword-arguments (package-arguments distrobox)
+       ((#:phases phases)
+	#~(modify-phases #$phases
+	    (replace 'refer-to-inputs
+	      (lambda* (#:key inputs #:allow-other-keys)
+		(copy-file "distrobox-init" "dinit") ; Temporarily move distrobox-init so it isn't affected by regex
+		(substitute* (find-files "." "^distrobox.*[^1]$")
+		  (("docker") (search-input-file inputs "/bin/docker"))
+		  (("wget") (search-input-file inputs "/bin/wget"))
+		  (("command -v") "test -x"))
+		(copy-file "dinit" "distrobox-init") ; move distrobox-init back
+		(delete-file "dinit")))))))))
+
+distrobox-docker
