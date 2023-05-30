0da6b5a81fa67ae5efd4dd011fc26d8de5ae084c
(lambda () nil) <shadenk30011@gmail.com>
2023-05-30 21:00:21 +0200

    add gnome.scm file

---
+++ configs-root/guix-modules/etc/guix-modules/nil/packages/gnome.scm
@@ -0,0 +1,20 @@
+(define-module (nil packages brogue)
+  #:use-module (guix)
+  #:use-module (gnu packages gnome))
+
+(define-public gnome-minimal
+  (package
+    (inherit gnome)
+    (name "gnome-minimal")
+    (propagated-inputs
+     (append
+      (modify-inputs (package-propagated-inputs gnome)
+	(delete "epiphany")
+	(delete "gnome-weather")
+	(delete "simple-scan")
+	(delete "gnome-maps")
+	(delete "cheese")
+	(delete "gnome-boxes")
+	(delete "gnome-console")
+	(delete "gnome-contacts")
+	(append gnome-terminal))))))
